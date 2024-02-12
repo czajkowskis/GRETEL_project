da804a2e6791ca1233ba56e24185c8668d6bdda5
czajkowskis <szymonczajkowski022@gmail.com>
2024-02-12 03:05:35 +0100

    Reduced number of optuna trials

--- src/explainer/generative/cf2.py
+++ src/explainer/generative/cf2.py
@@ -168,7 +168,7 @@ class CF2Explainer(Trainable, Explainer):
         self.context.logger.info("Performing hyperparameter search")
         sampler = optuna.samplers.TPESampler(seed=self.seed)
         study = optuna.create_study(study_name="CF2 optimization", sampler=sampler)
-        study.optimize(self.optuna_objective, n_trials=10)
+        study.optimize(self.optuna_objective, n_trials=5)
         self.context.logger.info(f"Best hyperparamteres found: {study.best_params}")
         return study.best_params
 
